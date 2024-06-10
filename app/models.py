0cce2534032cbf3350a1d69b2ab02123c7ad9470
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-10 17:04:16 +0300

    Creating the models

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from app import db
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    products = db.relationship('Product', backref='category', lazy=True)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
