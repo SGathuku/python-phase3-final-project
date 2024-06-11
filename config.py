5aa5e0807b1e336662180a2c1d0d8fbf4c256b43
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-11 08:12:59 +0300

    Database setup

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'mysql://username:password@localhost/liquor_store'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = 'your_secret_key'
