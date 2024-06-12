34eb77fbee743ce1da6905c7d2a650569f21cfd1
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-12 09:39:08 +0300

    Creating CLI that uses helper functions

---
+++ lib/cli.py
@@ -0,0 +1,39 @@
+from helpers import (
+    exit_program,
+    list_categories,
+    add_category,
+    list_products,
+    add_product
+)
+
+def main():
+    while True:
+        menu()
+        choice = input("> ")
+        if choice == "0":
+            exit_program()
+        elif choice == "1":
+            list_categories()
+        elif choice == "2":
+            name = input("Enter category name: ")
+            add_category(name)
+        elif choice == "3":
+            list_products()
+        elif choice == "4":
+            name = input("Enter product name: ")
+            price = input("Enter product price: ")
+            category_name = input("Enter category name: ")
+            add_product(name, price, category_name)
+        else:
+            print("Invalid choice")
+
+def menu():
+    print("Please select an option:")
+    print("0. Exit the program")
+    print("1. List categories")
+    print("2. Add a category")
+    print("3. List products")
+    print("4. Add a product")
+
+if __name__ == "__main__":
+    main()
